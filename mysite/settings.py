b7097ecbf9baad9ca37c0a961e20a996f7c4ca65
Troy <tathomas@email.wm.edu>
2018-08-12 14:51:01 -0700

    heroku attempt 10

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
