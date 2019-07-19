8ef0fa29dc6b4015019229d5e8aa73ea6751c8e7
Troy <tathomas@email.wm.edu>
2019-07-19 13:43:39 -0700

    Adding postgres as default db for local env

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,8 +89,12 @@ else:
 	DEBUG = True
 	DATABASES = {
 		'default': {
-		     'ENGINE': 'django.db.backends.sqlite3',
-		     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+			'ENGINE': 'django.db.backends.postgresql_psycopg2',
+			'NAME': 'local',
+			'USER': 'local',
+			'PASSWORD': 'local', 
+			'HOST': 'localhost',
+			'PORT': '5432',
 		}
 	}
 
