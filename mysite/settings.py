9e138765f49479b578361c28afb22f2f12ba2d9c
Troy <tathomas@email.wm.edu>
2019-09-03 07:49:37 -0700

    email settings again

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,8 +163,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'troyasherthomas'
+EMAIL_HOST_USER = 'api_key'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
