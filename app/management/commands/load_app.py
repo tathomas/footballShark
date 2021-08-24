c0fd374923e07c46cc6352c6ab932c0f18b85f14
Troy <tathomas@email.wm.edu>
2021-08-23 18:17:19 -0700

    Load 2021 games 1

--- app/management/commands/load_app.py
+++ app/management/commands/load_app.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 	help = 'Load in all team objects'
 
 	def load_teams(self):
-		input_data = team_list = ['Buffalo Bills', 'New York Jets', 'New England Patriots', 'Miami Dolphins', 'Pittsburgh Steelers', 'Baltimore Ravens', 'Cleveland Browns', 'Cincinnati Bengals', 'Houston Texans', 'Indianapolis Colts', 'Jacksonville Jaguars', 'Tennessee Titans', 'Denver Broncos', 'Kansas City Chiefs', 'Oakland Raiders', 'Los Angeles Chargers', 'Philadelphia Eagles', 'Dallas Cowboys', 'Washington Redskins', 'New York Giants', 'Tampa Bay Buccaneers', 'Carolina Panthers', 'Atlanta Falcons', 'New Orleans Saints', 'Green Bay Packers', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions', 'San Francisco 49ers', 'Seattle Seahawks', 'Arizona Cardinals', 'Los Angeles Rams']
+		input_data = team_list = ['Buffalo Bills', 'New York Jets', 'New England Patriots', 'Miami Dolphins', 'Pittsburgh Steelers', 'Baltimore Ravens', 'Cleveland Browns', 'Cincinnati Bengals', 'Houston Texans', 'Indianapolis Colts', 'Jacksonville Jaguars', 'Tennessee Titans', 'Denver Broncos', 'Kansas City Chiefs', 'Las Vegas Raiders', 'Los Angeles Chargers', 'Philadelphia Eagles', 'Dallas Cowboys', 'Washington Football Team', 'New York Giants', 'Tampa Bay Buccaneers', 'Carolina Panthers', 'Atlanta Falcons', 'New Orleans Saints', 'Green Bay Packers', 'Minnesota Vikings', 'Chicago Bears', 'Detroit Lions', 'San Francisco 49ers', 'Seattle Seahawks', 'Arizona Cardinals', 'Los Angeles Rams']
 
 		for team_name in input_data:
 			new_team, created = Team.objects.get_or_create(name=team_name)
@@ -24,7 +24,7 @@ class Command(BaseCommand):
 			team1 = Team.objects.get(name = line[6])
 			team2 = Team.objects.get(name = line[5])
 			
-			week, created = Week.objects.get_or_create(year=2018, num = line[1])
+			week, created = Week.objects.get_or_create(year=2021, num = line[1])
 
 			date_string = (str(line[3]) + str(line[0]) + str(line[4]))
 			date_time = datetime.strptime(date_string, '%B %d%Y%I:%M %p')
