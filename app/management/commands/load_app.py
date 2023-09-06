d11001ba69936a2b6791a71aad51c0e06b07b091
Troy <tathomas@email.wm.edu>
2023-09-06 11:00:53 -0700

    add requirements 32

--- app/management/commands/load_app.py
+++ app/management/commands/load_app.py
@@ -21,7 +21,10 @@ class Command(BaseCommand):
 		input_data = pd.read_csv("./app/management/Data/2023Games.csv", header=0).loc[:, :'Line']
 		my_data = input_data.drop(["Line"],axis=1)
 
-		for (ind, line), in numpy.ndenumerate(my_data.to_numpy()):
+		# for (ind, line), in numpy.ndenumerate(my_data.to_numpy()):
+		for ind, line in my_data.iterrows():
+
+
 			team1 = Team.objects.get(name = line[6])
 			team2 = Team.objects.get(name = line[5])
 			
